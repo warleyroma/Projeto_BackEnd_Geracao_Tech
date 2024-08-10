11a78c3ecca2d045cdad5051f4b77e4d0dcacf03
Warley Roma <warleyroma@gmail.com>
2024-08-10 17:58:09 -0300

    Configurando variaveis que irão guardar os dados de port e host

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const { createServer} = require('node:http');
 
+const host = '127.0.0.1';
+const port = 3000;
+
 const server = createServer((request, response) => {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
     response.end('Hello World');
 });
 
-server.listen(3000, '127.0.0.1', () => {
-    console.log("Servidor executando http://127.0.0.1:3000")
+server.listen(port, host, () => {
+    console.log(`PARABÉNS! Servidor executando http://${host}:${port}`)
 });
