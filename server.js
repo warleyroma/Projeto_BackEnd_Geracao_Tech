566000471ca4fbdb158367a8f8277e9b206f3d19
Warley Roma <warleyroma@gmail.com>
2024-08-10 17:44:58 -0300

    Configura servidor HTTP para escutar na porta 3000 e imprimir 'Hello World' como primeiro teste

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const { createServer} = require('node:http');
 
 const server = createServer((request, response) => {
     response.writeHead(200, {'Content-Type' : 'text/plain'});
-    request.end('Hello World');
+    response.end('Hello World');
 });
 
+server.listen(3000, '127.0.0.1', () => {
+    console.log("Servidor executando http://127.0.0.1:3000")
+});
