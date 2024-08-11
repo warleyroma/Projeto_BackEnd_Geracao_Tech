41aea6cc8245bab9793f47efbf695817ec458e78
Warley Roma <warleyroma@gmail.com>
2024-08-11 12:38:02 -0300

    Refatorando código para melhor organização

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
-const { createServer} = require('node:http');
+const { createServer } = require('node:http');
 
 const host = '127.0.0.1';
 const port = 3000;
 
 const server = createServer((request, response) => {
-    response.writeHead(200, {'Content-Type' : 'text/plain'});
-    response.end('Hello World');
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    return response.end('Teste com nodemon!');
 });
 
 server.listen(port, host, () => {
     console.log(`PARABÉNS! Servidor executando http://${host}:${port}`)
 });
+
+
+
