304b86a1fe30c6942a39b0c7fd091f2d3b19b597
Warley Roma <warleyroma@gmail.com>
2024-08-13 11:34:28 -0300

    implementadando função para adicionar produtos

---
+++ routes/produtos.js
@@ -0,0 +1,32 @@
+function listarProdutos() {
+    const produtos = [
+        {
+            id: 1,
+            nome: 'Mouse USB',
+            valor: 15.99
+        },
+
+        {
+            id: 2,
+            nome: 'Teclado USB',
+            valor: 29.99
+        },
+    ];
+    return produtos;
+};
+
+
+function addProdutos () {
+
+}
+
+function editarProdutos () {
+    
+}
+
+function excluirProdutos () {
+    
+}
+
+
+module.exports = listarProdutos; 
--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 const { createServer } = require('node:http');
+const listarProdutos = require('./routes/produtos');
 
 const host = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    return response.end('Teste com nodemon!');
+const app = createServer((request, response) => {
+
+    const { url, method } = request
+
+    if (url === '/'){
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        return response.end ("Olá NodeJS, servidor OK.");
+    }
+
+    
+    if (url === '/produtos'){
+
+        const dados = listarProdutos();
+
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        return response.end (JSON.stringify(dados));
+    }
+
+
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    return response.end('Página não encontrada');
 });
 
-server.listen(port, host, () => {
+app.listen(port, host, () => {
     console.log(`PARABÉNS! Servidor executando http://${host}:${port}`)
 });
 
