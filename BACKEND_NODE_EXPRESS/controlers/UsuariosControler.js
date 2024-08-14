54cea699e370497c6f32847597539357b54d2971
Warley Roma <warleyroma@gmail.com>
2024-08-14 16:18:53 -0300

    Implementando consultar por Id da classe UsuarioModel

--- BACKEND_NODE_EXPRESS/controlers/UsuariosControler.js
+++ BACKEND_NODE_EXPRESS/controlers/UsuariosControler.js
@@ -1,30 +1,41 @@
-const UsuarioModel = require('../models/UsuarioModel')
+const { json } = require("express");
+const UsuarioModel = require("../models/UsuarioModel");
 
 class UsuariosControler {
-    listar(){
-        const UsuarioModel = UsuarioModel();
-        return UsuarioModel.listar()
+    listar(request, response){
+        //const UsuarioModel = UsuarioModel();
+        //return UsuarioModel.listar()
+        const dados = UsuarioModel.listar()
+        return response.json(dados);
     }
 
-    consultarPorId(){
-        const UsuarioModel = UsuarioModel();
-        return UsuarioModel.consultarPorId()
+    consultarPorId(request, response){
+        //const UsuarioModel = UsuarioModel();
+        //return UsuarioModel.consultarPorId()
+        const id = request.params.id;
+        const dados = UsuarioModel.consultarPorId(id)
+        return response.json(dados);
     }
 
-    criar(){
+    criar(request, response){
 
-        const UsuarioModel = UsuarioModel();
-        return UsuarioModel.criar()
+       // const UsuarioModel = UsuarioModel();
+        //return UsuarioModel.criar()
+        const body = request.body;
+        UsuarioModel.criar(body);
+        return response.status(201).json({
+            message:"Usuário cadastrado com sucesso"
+        })
     }
 
-    atualizar(){
-        const UsuarioModel = UsuarioModel();
-        return UsuarioModel.atualizar()
+    atualizar(request, response){
+        //const UsuarioModel = UsuarioModel();
+        //return UsuarioModel.atualizar()
     }
 
-    deletar(){
-        const UsuarioModel = UsuarioModel();
-        return UsuarioModel.deletar()
+    deletar(request, response){
+        //const UsuarioModel = UsuarioModel();
+        //return UsuarioModel.deletar()
     }
 }
 
